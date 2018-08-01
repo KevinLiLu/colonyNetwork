35a4cde72dc33aa06b9c3b7918d679a5b2ad2821
lu.kevin@berkeley.edu <lu.kevin@berkeley.edu>
2018-08-01 10:35:30 -0700

    Enable test error msg check, and make tests pass

--- helpers/test-helper.js
+++ helpers/test-helper.js
@@ -105,8 +105,7 @@ export async function checkErrorRevert(promise, errorMessage) {
   } catch (err) {
     console.log(err);
     ({ tx, receipt, reason } = err);
-    // TODO: address as part of https://github.com/JoinColony/colonyNetwork/issues/201#issuecomment-407634292
-    // / assert.equal(reason, errorMessage);
+    assert.equal(reason, errorMessage);
   }
   // Check the receipt `status` to ensure transaction failed.
   assert.equal(receipt.status, 0x00);
